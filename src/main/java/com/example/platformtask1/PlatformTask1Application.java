04500cdeba629737ea2893cd04846634ed4a5b26
gprohorov <g.prokhorov@indevlab.com>
2023-07-04 15:18:46 +0300

    Initial commit

---
+++ src/main/java/com/example/platformtask1/PlatformTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.platformtask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformTask1Application.class, args);
+    }
+
+}
